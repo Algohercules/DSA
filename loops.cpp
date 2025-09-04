df4841a09115351dfb73c2078241259fcc2dbd40
Algohercules <adarshrreso@gmail.com>
2025-09-04 18:25:20 +0530

    pattern 1

--- loops.cpp
+++ loops.cpp
@@ -1,6 +1,37 @@
 //first step 
 /// for the outer loop count the no of rows 
-// for the inner loop  , focus on the rows and the n try them to anyhiw connect ot the rows 
+// for the inner loop  , focus on the rows and the n try them to anyhow connect ot the rows 
+// print what ever is asked for 
+
+#include<iostream>
+using namespace std;
+
+
+//! first program with test cases 
+//  void print(int n){
+//   for(int i = 0; i<n; i++){
+//     for( int j=0; j<n; j++){
+//         cout << "*";
+//     }
+//     cout << endl;
+//   }
+// }
+
+
+// int main(){
+//     int t;
+//     cin >> t;
+//     for( int i = 0; i<t; i++){
+//         int n;
+//         cin >> n;
+//         print(n);
+//     }
+
+
+// }
+
+
+//! second loop
 
 
 
