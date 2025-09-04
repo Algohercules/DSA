4f8be959d9f08c285891eaaa4fe11027c902af27
Algohercules <adarshrreso@gmail.com>
2025-09-04 18:36:32 +0530

    3

--- loops.cpp
+++ loops.cpp
@@ -27,9 +27,30 @@ cout << endl;
  }
 
  void print3(int n){
-    
+    for(int i=1; i<n; i++){
+        for(int j=1; j<i; j++){
+            cout<< j << " ";
+        }
+        cout<< endl;
+    }
  }
-int main(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ int main(){
     int t;
     cin >> t;
     for( int i = 0; i<t; i++){
